74e01cb3f589e88cb8ae10b53e971587481fb6ee
mehtesham3 <ehteshampersonal38@gmail.com>
2025-09-24 09:25:46 +0530

    Port number updated

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Hii Everyone </h1>&ns<code>~It's Ehtesham</code>");
 })
 
-app.listen(3400, () => {
-  console.log("Server is running on port no. http://localhost:3400/");
+const port = 3400;
+
+app.listen(port, () => {
+  console.log(`Server is running on port no. http://localhost:${port}/`);
 })
