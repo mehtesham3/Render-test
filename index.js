c530c6de0dd944e0d055d393925753d67d2b4b83
Mohammad-Ehtesham <ehteshamm595@gmail.com>
2025-09-24 08:00:23 +0530

    Index file Updated

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express()
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hii Everyone !!</h1>");
+  res.send("<h1>Hii Everyone </h1>&ns<code>~It's Ehtesham</code>");
+})
+
+app.listen(3400, () => {
+  console.log("Server is running on port no. http://localhost:3400/");
 })
